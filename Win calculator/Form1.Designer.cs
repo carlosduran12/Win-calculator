bc634a3b9909ac5304d98dd89391c7929bcf602e
carlosduran12 <60364528+carlosduran12@users.noreply.github.com>
2021-04-21 13:14:45 -0400

    Add project files.

---
+++ Win calculator/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿
+namespace Win_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnClick1 = new System.Windows.Forms.Button();
+            this.btnClick4 = new System.Windows.Forms.Button();
+            this.btnClick7 = new System.Windows.Forms.Button();
+            this.btnClick8 = new System.Windows.Forms.Button();
+            this.btnClick5 = new System.Windows.Forms.Button();
+            this.btnClick2 = new System.Windows.Forms.Button();
+            this.btnClick3 = new System.Windows.Forms.Button();
+            this.btnClick6 = new System.Windows.Forms.Button();
+            this.btnClick9 = new System.Windows.Forms.Button();
+            this.btnClickDot = new System.Windows.Forms.Button();
+            this.btnClick0 = new System.Windows.Forms.Button();
+            this.btnClickC = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnClickPlus = new System.Windows.Forms.Button();
+            this.btnClickSub = new System.Windows.Forms.Button();
+            this.btnClickMulti = new System.Windows.Forms.Button();
+            this.btnClickDivide = new System.Windows.Forms.Button();
+            this.btnClickEquals = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.25F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.88889F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.11111F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel2.Controls.Add(this.btnClick1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnClick4, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btnClick7, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.btnClick8, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.btnClick5, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btnClick2, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnClick3, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnClick6, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btnClick9, 2, 2);
+            this.tableLayoutPanel2.Controls.Add(this.btnClickDot, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.btnClick0, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.btnClickC, 2, 3);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 97);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(584, 350);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // btnClick1
+            // 
+            this.btnClick1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClick1.Location = new System.Drawing.Point(3, 3);
+            this.btnClick1.Name = "btnClick1";
+            this.btnClick1.Size = new System.Drawing.Size(188, 81);
+            this.btnClick1.TabIndex = 0;
+            this.btnClick1.Text = "1";
+            this.btnClick1.UseVisualStyleBackColor = true;
+            this.btnClick1.Click += new System.EventHandler(this.btnClick1_Click);
+            // 
+            // btnClick4
+            // 
+            this.btnClick4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClick4.Location = new System.Drawing.Point(3, 90);
+            this.btnClick4.Name = "btnClick4";
+            this.btnClick4.Size = new System.Drawing.Size(188, 81);
+            this.btnClick4.TabIndex = 1;
+            this.btnClick4.Text = "4";
+            this.btnClick4.UseVisualStyleBackColor = true;
+            this.btnClick4.Click += new System.EventHandler(this.btnClick4_Click);
+            // 
+            // btnClick7
+            // 
+            this.btnClick7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClick7.Location = new System.Drawing.Point(3, 177);
+            this.btnClick7.Name = "btnClick7";
+            this.btnClick7.Size = new System.Drawing.Size(188, 81);
+            this.btnClick7.TabIndex = 2;
+            this.btnClick7.Text = "7";
+            this.btnClick7.UseVisualStyleBackColor = true;
+            this.btnClick7.Click += new System.EventHandler(this.btnClick7_Click);
+            // 
+            // btnClick8
+            // 
+            this.btnClick8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClick8.Location = new System.Drawing.Point(197, 177);
+            this.btnClick8.Name = "btnClick8";
+            this.btnClick8.Size = new System.Drawing.Size(188, 81);
+            this.btnClick8.TabIndex = 3;
+            this.btnClick8.Text = "8";
+            this.btnClick8.UseVisualStyleBackColor = true;
+            this.btnClick8.Click += new System.EventHandler(this.btnClick8_Click);
+            // 
+            // btnClick5
+            // 
+            this.btnClick5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClick5.Location = new System.Drawing.Point(197, 90);
+            this.btnClick5.Name = "btnClick5";
+            this.btnClick5.Size = new System.Drawing.Size(188, 81);
+            this.btnClick5.TabIndex = 4;
+            this.btnClick5.Text = "5";
+            this.btnClick5.UseVisualStyleBackColor = true;
+            this.btnClick5.Click += new System.EventHandler(this.btnClick5_Click);
+            // 
+            // btnClick2
+            // 
+            this.btnClick2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClick2.Location = new System.Drawing.Point(197, 3);
+            this.btnClick2.Name = "btnClick2";
+            this.btnClick2.Size = new System.Drawing.Size(188, 81);
+            this.btnClick2.TabIndex = 5;
+            this.btnClick2.Text = "2";
+            this.btnClick2.UseVisualStyleBackColor = true;
+            this.btnClick2.Click += new System.EventHandler(this.btnClick2_Click);
+            // 
+            // btnClick3
+            // 
+            this.btnClick3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClick3.Location = new System.Drawing.Point(391, 3);
+            this.btnClick3.Name = "btnClick3";
+            this.btnClick3.Size = new System.Drawing.Size(190, 81);
+            this.btnClick3.TabIndex = 6;
+            this.btnClick3.Text = "3";
+            this.btnClick3.UseVisualStyleBackColor = true;
+            this.btnClick3.Click += new System.EventHandler(this.btnClick3_Click);
+            // 
+            // btnClick6
+            // 
+            this.btnClick6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClick6.Location = new System.Drawing.Point(391, 90);
+            this.btnClick6.Name = "btnClick6";
+            this.btnClick6.Size = new System.Drawing.Size(190, 81);
+            this.btnClick6.TabIndex = 7;
+            this.btnClick6.Text = "6";
+            this.btnClick6.UseVisualStyleBackColor = true;
+            this.btnClick6.Click += new System.EventHandler(this.btnClick6_Click);
+            // 
+            // btnClick9
+            // 
+            this.btnClick9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClick9.Location = new System.Drawing.Point(391, 177);
+            this.btnClick9.Name = "btnClick9";
+            this.btnClick9.Size = new System.Drawing.Size(190, 81);
+            this.btnClick9.TabIndex = 8;
+            this.btnClick9.Text = "9";
+            this.btnClick9.UseVisualStyleBackColor = true;
+            this.btnClick9.Click += new System.EventHandler(this.btnClick9_Click);
+            // 
+            // btnClickDot
+            // 
+            this.btnClickDot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClickDot.Location = new System.Drawing.Point(3, 264);
+            this.btnClickDot.Name = "btnClickDot";
+            this.btnClickDot.Size = new System.Drawing.Size(188, 83);
+            this.btnClickDot.TabIndex = 9;
+            this.btnClickDot.Text = ".";
+            this.btnClickDot.UseVisualStyleBackColor = true;
+            this.btnClickDot.Click += new System.EventHandler(this.btnClickDot_Click);
+            // 
+            // btnClick0
+            // 
+            this.btnClick0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClick0.Location = new System.Drawing.Point(197, 264);
+            this.btnClick0.Name = "btnClick0";
+            this.btnClick0.Size = new System.Drawing.Size(188, 83);
+            this.btnClick0.TabIndex = 10;
+            this.btnClick0.Text = "0";
+            this.btnClick0.UseVisualStyleBackColor = true;
+            this.btnClick0.Click += new System.EventHandler(this.btnClick0_Click);
+            // 
+            // btnClickC
+            // 
+            this.btnClickC.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClickC.Location = new System.Drawing.Point(391, 264);
+            this.btnClickC.Name = "btnClickC";
+            this.btnClickC.Size = new System.Drawing.Size(190, 83);
+            this.btnClickC.TabIndex = 11;
+            this.btnClickC.Text = "C";
+            this.btnClickC.UseVisualStyleBackColor = true;
+            this.btnClickC.Click += new System.EventHandler(this.btnClickC_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.btnClickPlus, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnClickSub, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.btnClickMulti, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.btnClickDivide, 0, 3);
+            this.tableLayoutPanel3.Controls.Add(this.btnClickEquals, 0, 4);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(593, 97);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 5;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(204, 350);
+            this.tableLayoutPanel3.TabIndex = 4;
+            // 
+            // btnClickPlus
+            // 
+            this.btnClickPlus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClickPlus.Location = new System.Drawing.Point(3, 3);
+            this.btnClickPlus.Name = "btnClickPlus";
+            this.btnClickPlus.Size = new System.Drawing.Size(198, 64);
+            this.btnClickPlus.TabIndex = 0;
+            this.btnClickPlus.Text = "+";
+            this.btnClickPlus.UseVisualStyleBackColor = true;
+            this.btnClickPlus.Click += new System.EventHandler(this.btnClickPlus_Click);
+            // 
+            // btnClickSub
+            // 
+            this.btnClickSub.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClickSub.Location = new System.Drawing.Point(3, 73);
+            this.btnClickSub.Name = "btnClickSub";
+            this.btnClickSub.Size = new System.Drawing.Size(198, 64);
+            this.btnClickSub.TabIndex = 1;
+            this.btnClickSub.Text = "-";
+            this.btnClickSub.UseVisualStyleBackColor = true;
+            this.btnClickSub.Click += new System.EventHandler(this.btnClickSub_Click);
+            // 
+            // btnClickMulti
+            // 
+            this.btnClickMulti.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClickMulti.Location = new System.Drawing.Point(3, 143);
+            this.btnClickMulti.Name = "btnClickMulti";
+            this.btnClickMulti.Size = new System.Drawing.Size(198, 64);
+            this.btnClickMulti.TabIndex = 2;
+            this.btnClickMulti.Text = "*";
+            this.btnClickMulti.UseVisualStyleBackColor = true;
+            this.btnClickMulti.Click += new System.EventHandler(this.btnClickMulti_Click);
+            // 
+            // btnClickDivide
+            // 
+            this.btnClickDivide.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClickDivide.Location = new System.Drawing.Point(3, 213);
+            this.btnClickDivide.Name = "btnClickDivide";
+            this.btnClickDivide.Size = new System.Drawing.Size(198, 64);
+            this.btnClickDivide.TabIndex = 3;
+            this.btnClickDivide.Text = "/";
+            this.btnClickDivide.UseVisualStyleBackColor = true;
+            this.btnClickDivide.Click += new System.EventHandler(this.btnClickDivide_Click);
+            // 
+            // btnClickEquals
+            // 
+            this.btnClickEquals.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClickEquals.Location = new System.Drawing.Point(3, 283);
+            this.btnClickEquals.Name = "btnClickEquals";
+            this.btnClickEquals.Size = new System.Drawing.Size(198, 64);
+            this.btnClickEquals.TabIndex = 4;
+            this.btnClickEquals.Text = "=";
+            this.btnClickEquals.UseVisualStyleBackColor = true;
+            this.btnClickEquals.Click += new System.EventHandler(this.btnClickEquals_Click);
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 2);
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(794, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnClick1;
+        private System.Windows.Forms.Button btnClick4;
+        private System.Windows.Forms.Button btnClick7;
+        private System.Windows.Forms.Button btnClick8;
+        private System.Windows.Forms.Button btnClick5;
+        private System.Windows.Forms.Button btnClick2;
+        private System.Windows.Forms.Button btnClick3;
+        private System.Windows.Forms.Button btnClick6;
+        private System.Windows.Forms.Button btnClick9;
+        private System.Windows.Forms.Button btnClickDot;
+        private System.Windows.Forms.Button btnClick0;
+        private System.Windows.Forms.Button btnClickC;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnClickPlus;
+        private System.Windows.Forms.Button btnClickSub;
+        private System.Windows.Forms.Button btnClickMulti;
+        private System.Windows.Forms.Button btnClickDivide;
+        private System.Windows.Forms.Button btnClickEquals;
+    }
+}
+
---
+++ Win calculator/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClick1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClick2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClick3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClick4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClick5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClick6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClick7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClick8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClick9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClickDot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClick0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClickC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClickPlus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClickSub_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClickMulti_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClickDivide_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClickEquals_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
